c3e5c824bd8cb776b84def8e08688c78602ea790
gerynsb <118332285+gerynsb@users.noreply.github.com>
2025-03-23 13:48:51 +0800

    Setup : Footer

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,33 +1,11 @@
+import { Footer } from "../../components/footer/Footer";
+import { Navbar } from "../../components/navbar/Navbar";
 import styles from "./homepage.module.css";
 import Link from "next/link";
 
 export default function Home() {
   return <div>
-    <Link href='/'>
-    <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-      Similique ipsa minus quos vel nulla placeat totam cupiditate veritatis repudiandae asperiores, 
-      unde atque consectetur, laboriosam tempora. Possimus ab amet minima repudiandae?</h1>
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-    Est fugiat quaerat ab adipisci obcaecati tenetur nisi deleniti vel ratione praesentium incidunt, 
-    consectetur officia eligendi veniam perferendis. Dignissimos natus eum ex.
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-    Est fugiat quaerat ab adipisci obcaecati tenetur nisi deleniti vel ratione praesentium incidunt, 
-    consectetur officia eligendi veniam perferendis. Dignissimos natus eum ex.
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-    Est fugiat quaerat ab adipisci obcaecati tenetur nisi deleniti vel ratione praesentium incidunt, 
-    consectetur officia eligendi veniam perferendis. Dignissimos natus eum ex.
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-    Est fugiat quaerat ab adipisci obcaecati tenetur nisi deleniti vel ratione praesentium incidunt, 
-    consectetur officia eligendi veniam perferendis. Dignissimos natus eum ex.
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-    Est fugiat quaerat ab adipisci obcaecati tenetur nisi deleniti vel ratione praesentium incidunt, 
-    consectetur officia eligendi veniam perferendis. Dignissimos natus eum ex.
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-    Est fugiat quaerat ab adipisci obcaecati tenetur nisi deleniti vel ratione praesentium incidunt, 
-    consectetur officia eligendi veniam perferendis. Dignissimos natus eum ex.
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-    Est fugiat quaerat ab adipisci obcaecati tenetur nisi deleniti vel ratione praesentium incidunt, 
-    consectetur officia eligendi veniam perferendis. Dignissimos natus eum ex.
-    </Link>
+    <Navbar/>
+    <Footer/>
   </div>;
 }
