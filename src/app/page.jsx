01c8dad2f83b0f2d0d9de164f411ce8c12088a52
gerynsb <118332285+gerynsb@users.noreply.github.com>
2025-03-23 13:31:40 +0800

    set up awal

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,33 @@
 import styles from "./homepage.module.css";
+import Link from "next/link";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return <div>
+    <Link href='/'>
+    <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+      Similique ipsa minus quos vel nulla placeat totam cupiditate veritatis repudiandae asperiores, 
+      unde atque consectetur, laboriosam tempora. Possimus ab amet minima repudiandae?</h1>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+    Est fugiat quaerat ab adipisci obcaecati tenetur nisi deleniti vel ratione praesentium incidunt, 
+    consectetur officia eligendi veniam perferendis. Dignissimos natus eum ex.
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+    Est fugiat quaerat ab adipisci obcaecati tenetur nisi deleniti vel ratione praesentium incidunt, 
+    consectetur officia eligendi veniam perferendis. Dignissimos natus eum ex.
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+    Est fugiat quaerat ab adipisci obcaecati tenetur nisi deleniti vel ratione praesentium incidunt, 
+    consectetur officia eligendi veniam perferendis. Dignissimos natus eum ex.
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+    Est fugiat quaerat ab adipisci obcaecati tenetur nisi deleniti vel ratione praesentium incidunt, 
+    consectetur officia eligendi veniam perferendis. Dignissimos natus eum ex.
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+    Est fugiat quaerat ab adipisci obcaecati tenetur nisi deleniti vel ratione praesentium incidunt, 
+    consectetur officia eligendi veniam perferendis. Dignissimos natus eum ex.
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+    Est fugiat quaerat ab adipisci obcaecati tenetur nisi deleniti vel ratione praesentium incidunt, 
+    consectetur officia eligendi veniam perferendis. Dignissimos natus eum ex.
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+    Est fugiat quaerat ab adipisci obcaecati tenetur nisi deleniti vel ratione praesentium incidunt, 
+    consectetur officia eligendi veniam perferendis. Dignissimos natus eum ex.
+    </Link>
+  </div>;
 }
